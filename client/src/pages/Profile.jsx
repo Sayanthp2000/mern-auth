6c657314d4ee5a79df0fb45c00a174a7ca1cf8aa
Sayanthp2000 <psaya1852@gmail.com>
2024-10-01 07:39:08 +0530

    add signout user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -103,6 +103,15 @@ function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      await fetch('/api/auth/signout');
+      dispatch(signOut())
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -130,7 +139,7 @@ function Profile() {
        </form>
          <div className='flex justify-between mt-5'>
             <span className='text-red-700 cursor-pointer' onClick={handleDeleteAccount}>Delete Account</span>
-            <span className='text-red-700 cursor-pointer'>Sign out</span>
+            <span className='text-red-700 cursor-pointer' onClick={handleSignOut}>Sign out</span>
          </div>
          <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
       <p className='text-green-700 mt-5'>
